6b0ae4e83aec6518214fbf705a832d263d5df98a
echo '# Set PATH, MANPATH, etc., for Homebrew.'  /Users/daliaali/.zprofile <dnabil89@gmail.com>
2023-01-03 13:54:47 -0800

    wave1: added video and customer models, created the .env file

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,4 +1,10 @@
 from app import db
 
 class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    postal_code = db.Column(db.Integer, nullable=False)
+    phone_number = db.Column(db.String, nullable=False)
+    register_at = db.Column(db.DateTime, nullable=False)
+    videos_checked_out_count = db.Column(db.Integer, nullable=False)
+
--- app/models/video.py
+++ app/models/video.py
@@ -1,4 +1,8 @@
 from app import db
 
 class Video(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    total_inventory = db.Column(db.Integer, nullable=False)
+    release_date = db.Column(db.DateTime)
+    
