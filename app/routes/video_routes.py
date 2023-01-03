4d27307e9bbdec0b58f106b0afeec15eac7ee9a8
nadxelle <nadxellehernandez@gmail.com>
2023-01-03 15:36:14 -0800

    finished get video by id

--- app/routes/video_routes.py
+++ app/routes/video_routes.py
@@ -11,4 +11,23 @@ def get_all_videos():
     videos_response = [video.to_dict() for video in videos]
 
     return jsonify(videos_response)
-    
+
+def validate_video_id(video_id):
+    try:
+        id = int(video_id)
+    except:
+        msg = f"Video id: {video_id} is Invalid"
+        abort(make_response(jsonify({"details": msg}),400))
+
+    video = Video.query.get(id)
+
+    if video:
+        return video
+
+    msg = f"Video {video_id} was not found"
+    abort(make_response(jsonify({"message": msg}),404))
+
+@videos_bp.route("/<video_id>",methods=["GET"])
+def get_video(video_id):
+    video = validate_video_id(video_id)
+    return jsonify(video.to_dict())    
