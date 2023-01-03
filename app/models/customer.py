7066edc3fea37ccff7dfe9f5298f6c6adc558403
echo '# Set PATH, MANPATH, etc., for Homebrew.'  /Users/daliaali/.zprofile <dnabil89@gmail.com>
2023-01-03 14:04:47 -0800

    changed postal code type to string

--- app/models/customer.py
+++ app/models/customer.py
@@ -3,7 +3,7 @@ from app import db
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     name = db.Column(db.String, nullable=False)
-    postal_code = db.Column(db.Integer, nullable=False)
+    postal_code = db.Column(db.String, nullable=False)
     phone_number = db.Column(db.String, nullable=False)
     register_at = db.Column(db.DateTime, nullable=False)
     videos_checked_out_count = db.Column(db.Integer, nullable=False)
