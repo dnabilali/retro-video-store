ddc388bf946234da1632f0620640dfb14fed91be
nadxelle <nadxellehernandez@gmail.com>
2023-01-04 11:32:09 -0800

    Finished all the crud for video model with all tests passing

--- app/routes/video_routes.py
+++ app/routes/video_routes.py
@@ -28,6 +28,29 @@ def validate_video_id(video_id):
     msg = f"Video {video_id} was not found"
     abort(make_response(jsonify({"message": msg}),404))
 
+def validate_request_body(request_body):
+    if not request_body:
+        msg = "An empty or invalid json object was sent."
+        abort(make_response(jsonify({"message":msg}),400))
+
+    req_title = request_body.get("title")
+    req_total_inventory = request_body.get("total_inventory")
+    req_release_date = request_body.get("release_date") 
+
+    if not req_title:
+        msg = "Request body must include title."
+        abort(make_response(jsonify({"details":msg}),400))
+
+    if not req_total_inventory or type(req_total_inventory) is not int:
+        msg = "Request body must include total_inventory."
+        abort(make_response(jsonify({"details":msg}),400))
+
+    if not req_release_date:
+        msg = "Request body must include release_date."
+        abort(make_response(jsonify({"details":msg}),400))
+
+    return req_title,req_total_inventory,req_release_date
+
 @videos_bp.route("/<video_id>",methods=["GET"])
 def get_video(video_id):
     video = validate_video_id(video_id)
@@ -36,31 +59,12 @@ def get_video(video_id):
 @videos_bp.route("",methods=["POST"])
 def create_video():
     request_body = request.get_json(silent=True)
-    if not request_body:
-        msg = "An empty or invalid json object was sent. Can't create video"
-        abort(make_response(jsonify({"message":msg}),400))
-
-   
-    req_title = request_body.get("title")
-    req_total_inventory = request_body.get("total_inventory")
-    req_release_date = request_body.get("release_date") 
-
-    if not req_title:
-        msg = "Request body must include title. Can't create video"
-        abort(make_response(jsonify({"details":msg}),400))
-
-    if not req_total_inventory:
-        msg = "Request body must include total_inventory. Can't create video"
-        abort(make_response(jsonify({"details":msg}),400))
-
-    if not req_release_date:
-        msg = "Request body must include release_date. Can't create video"
-        abort(make_response(jsonify({"details":msg}),400))
+    request_data = validate_request_body(request_body)
 
     new_video = Video(
-            title = req_title,
-            total_inventory = req_total_inventory,
-            release_date = req_release_date
+            title = request_data[0],
+            total_inventory = request_data[1],
+            release_date = request_data[2]
     )
 
     try:
@@ -72,3 +76,27 @@ def create_video():
         abort(make_response(jsonify({"message":msg}),500))
 
     return make_response(jsonify(new_video.to_dict()),201)
+
+@videos_bp.route("/<video_id>",methods=["PUT"])    
+def update_video(video_id):
+    video = validate_video_id(video_id)
+
+    request_body = request.get_json(silent=True)
+    request_data = validate_request_body(request_body)
+
+    video.title = request_data[0]
+    video.total_inventory = request_data[1]
+    video.release_date = request_data[2]
+
+    db.session.commit()
+
+    return make_response(jsonify(video.to_dict()),200)
+
+@videos_bp.route("/<video_id>",methods=["DELETE"]) 
+def delete_video(video_id):
+    video = validate_video_id(video_id)
+
+    db.session.delete(video)
+    db.session.commit()
+
+    return make_response(jsonify({"id":video.id}),200)
